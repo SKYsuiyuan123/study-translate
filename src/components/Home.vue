f1dee2ef606bee5cc9d59363145b49e0abbec053
sunpeiyuan <sunpeiyuan@aicyber.com>
2018-07-21 16:57:11 +0800

    在线翻译完成，并且使用 Vuex 来存储数据

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>在线翻译</h1>
+    <h5>简单 / 易容 / 便捷</h5>
+    <translate-form></translate-form>
+    <translate-output></translate-output>
+  </div>
+</template>
+
+<script>
+import translateForm from './translateForm'
+import translateOutput from './translateOutput'
+
+export default {
+  data () {
+    return {}
+  },
+  components: {
+    translateForm,
+    translateOutput
+  }
+}
+</script>
---
+++ src/components/translateForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="translateForm">
+    <h5>这是输入</h5>
+    <form @submit.prevent="formSubmit">
+      <input v-model="translateText" type="text" placeholder="请输入你要翻译的内容">
+      <select v-model="language" @change="formSubmit">
+        <option value="en">English</option>
+        <option value="ru">Russian</option>
+        <option value="ko">Korean</option>
+        <option value="ja">Janpenese</option>
+      </select>
+      <input type="submit" value="翻译">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      translateText: '',
+      language: 'en'
+    }
+  },
+  methods: {
+    formSubmit () {
+      // 将数据存储到 store 中
+      this.$store.commit('modifyLanguage', this.language)
+      this.$store.commit('addTranslateText', this.translateText)
+    }
+  }
+}
+</script>
